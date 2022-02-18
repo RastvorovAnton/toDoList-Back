b1ad5f97e217b4c9fbc09ea6850a2bcbd3326837
NataliaShchelkunova <92028211+NataliaShchelkunova@users.noreply.github.com>
2022-02-18 21:53:54 +0300

    Update task.controller.js

--- src/modules/controllers/task.controller.js
+++ src/modules/controllers/task.controller.js
@@ -9,38 +9,38 @@ module.exports.getAllTasks = (req, res, next) => {
 module.exports.createNewTask = (req, res, next) => {
 	if (req.body.hasOwnProperty('text') && req.body.hasOwnProperty('isCheck')) {
 		const task = new Task(req.body);
-		task
+		task //whats happend with code style?
 			.save()
 			.then((result) => {
 				res.send(result);
 			})
-			.catch((err) => console.log(err));
+			.catch((err) => console.log(err)); // res.status(number).send('message');
 	} else {
 		res.status(404).send('Error');
 	}
 };
 
-module.exports.deleteTask = (req, res, next) => {
+module.exports.deleteTask = (req, res, next) => { //do you need next?
 	if (req.query.id) {
-		Task.deleteOne({ _id: req.query.id })
+		Task.deleteOne({ _id: req.query.id }) // destructure req.query.id
 			.then((result) => {
 				Task.find().then((result) => {
 					res.send(result);
 				});
 			})
-			.catch((err) => console.log(err));
+			.catch((err) => console.log(err)); // look at 17 string
 	} else {
 		res.status(404).send('Error');
 	}
 };
 
-module.exports.changeTaskInfo = (req, res, next) => {
-  if (req.query.hasOwnProperty('id') && (req.body.hasOwnProperty('text') || req.body.hasOwnProperty('isCheck'))) {
-    Task.updateOne({ _id: req.query.id }, req.body).then((result) => {
+module.exports.changeTaskInfo = (req, res, next) => { // look at 23 str
+  if (req.query.hasOwnProperty('id') && (req.body.hasOwnProperty('text') || req.body.hasOwnProperty('isCheck'))) { //it is uncorrect condition
+    Task.updateOne({ _id: req.query.id }, req.body).then((result) => { // destructure req.query.id 
       Task.find().then((result) => {
         res.send({ data: result });
       });
-    });
+    }); // why u don't use .catch()?
   } else {
     res.status(404).send('Somthing wrong');
   }
